5c179e8b4503a4fa7357e950e641cee7bf3e00c6
hanjeahwan <hanjeahwan@gmail.com>
2018-12-26 23:00:11 +0800

    initial

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,12 @@
-// workbox.setConfig({
-//   "debug": true
-// })
+workbox.setConfig({
+    "debug": true
+})
 
 // set the prefix and suffix of our sw's name
 workbox.core.setCacheNameDetails({
     prefix: 'vue-pwa-app',
     suffix: 'v1.0.0',
 });
-
-// have our sw update and control a web page as soon as possible.
-workbox.skipWaiting();
-workbox.clientsClaim();
 
 // vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
@@ -20,14 +16,15 @@ workbox.routing.registerNavigationRoute('/index.html');
 
 workbox.routing.registerRoute(new RegExp('https://nestjs-pulsifi.herokuapp.com/.*'), workbox.strategies.networkFirst({}), 'GET')
 
-const bgSyncPlugin = new workbox.backgroundSync.Plugin('SyncQueue', {
-    maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
-});
+const bgSyncPlugin = new workbox.backgroundSync.Plugin('myQueueName');
 
 workbox.routing.registerRoute(
-    'https://nestjs-pulsifi.herokuapp.com/*',
-    workbox.strategies.networkOnly({
-        plugins: [bgSyncPlugin]
+    new RegExp('https://nestjs-pulsifi.herokuapp.com/.*'),
+    new workbox.strategies.networkOnly({
+        plugins: [bgSyncPlugin],
     }),
-    'POST'
 );
+
+// have our sw update and control a web page as soon as possible.
+workbox.skipWaiting();
+workbox.clientsClaim();
